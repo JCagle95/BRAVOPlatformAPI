0102ead229cfbb79a0d93f2c5261e0e756be1c47
Jackson Cagle <JCagle95@users.noreply.github.com>
2026-06-11 06:53:58 -0600

    Delete src/bravo directory

--- src/bravo/client/__init__.py
+++
@@ -1,4 +0,0 @@
-from .BRAVOPlatformRequest import BRAVOPlatformRequest, DefaultConfigurations
-
-# Optional: This defines what gets imported if a user does 'from bravo_api import *'
-__all__ = ["BRAVOPlatformRequest", "DefaultConfigurations"]
